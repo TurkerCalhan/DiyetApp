9171cceecf63a1c19ed22a39eff5a8f6e85729aa
TurkerCalhan <turker.calhan@outlook.com>
2023-02-10 12:30:11 +0300

    Add project files.

---
+++ DiyetApp/Controllers/DiyetController.cs
@@ -0,0 +1,32 @@
+﻿using DiyetApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DiyetApp.Controllers
+{
+    public class DiyetController : Controller
+    {
+        // GET: Diyet
+        public ActionResult Index()
+        {
+            return View(Veritabani.Liste);
+        }
+
+        [HttpGet]
+        public ActionResult Ekle()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Ekle(Diyet entity)
+        {
+            entity.Tarih=DateTime.Now;
+            Veritabani.Ekle(entity);
+            return View("Index",Veritabani.Liste);
+        }
+    }
+}
---
+++ DiyetApp/Models/Veritabani.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DiyetApp.Models
+{
+    public static class Veritabani
+    {
+        private static List<Diyet> _liste;
+
+        static Veritabani()
+        {
+            _liste = new List<Diyet>()
+            {
+                new Diyet(){Ogun="Kahvaltı",Aciklama="Peynir,Zeytin ve 1 Yumurta", Tarih=DateTime.Now.AddDays(-1),Tamamlandimi=true},
+                new Diyet(){Ogun="Ara Öğün",Aciklama="1 Elma", Tarih=DateTime.Now.AddDays(-1),Tamamlandimi=true},
+                new Diyet(){Ogun="Öğle Yemeği",Aciklama="Zeytinyağlı fasulye, Yoğurt ve Yağsız Salata", Tarih=DateTime.Now.AddDays(-1),Tamamlandimi=true},
+                 new Diyet(){Ogun="Ara Öğün",Aciklama="1 Armut", Tarih=DateTime.Now.AddDays(-1),Tamamlandimi=true},
+                  new Diyet(){Ogun="Akşam Yemeği",Aciklama="Çorba Tavuk ve Salata", Tarih=DateTime.Now.AddDays(-1),Tamamlandimi=true},
+
+
+                  new Diyet(){Ogun="Kahvaltı",Aciklama="Peynir,Zeytin ve 1 Yumurta", Tarih=DateTime.Now.AddDays(0),Tamamlandimi=false},
+                new Diyet(){Ogun="Ara Öğün",Aciklama="1 Elma", Tarih=DateTime.Now.AddDays(0),Tamamlandimi=false},
+                new Diyet(){Ogun="Öğle Yemeği",Aciklama="Zeytinyağlı fasulye, Yoğurt ve Yağsız Salata", Tarih=DateTime.Now.AddDays(0),Tamamlandimi=false},
+                 new Diyet(){Ogun="Ara Öğün",Aciklama="1 Armut", Tarih=DateTime.Now.AddDays(0),Tamamlandimi=false},
+                  new Diyet(){Ogun="Akşam Yemeği",Aciklama="Çorba Tavuk ve Salata", Tarih=DateTime.Now.AddDays(0),Tamamlandimi=false},
+
+
+                  new Diyet(){Ogun="Kahvaltı",Aciklama="Peynir,Zeytin ve 1 Yumurta", Tarih=DateTime.Now.AddDays(1),Tamamlandimi=false},
+                new Diyet(){Ogun="Ara Öğün",Aciklama="1 Elma", Tarih=DateTime.Now.AddDays(1),Tamamlandimi=false},
+                new Diyet(){Ogun="Öğle Yemeği",Aciklama="Zeytinyağlı fasulye, Yoğurt ve Yağsız Salata", Tarih=DateTime.Now.AddDays(1),Tamamlandimi=false},
+                 new Diyet(){Ogun="Ara Öğün",Aciklama="1 Armut", Tarih=DateTime.Now.AddDays(1),Tamamlandimi=false},
+                  new Diyet(){Ogun="Akşam Yemeği",Aciklama="Çorba Tavuk ve Salata", Tarih=DateTime.Now.AddDays(1),Tamamlandimi=false},
+            };
+        }
+
+        public static List<Diyet> Liste
+        {
+            get { return _liste; }
+        }
+
+        public static void Ekle(Diyet diyet)
+        {
+            _liste.Add(diyet);
+        }
+    }
+}
